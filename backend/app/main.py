483c001399efbd0ab7d32b509464f3b54d032c18
danial_rube <dnlnorozi@gmail.com>
2026-07-11 21:59:53 +0330

    API's to "create new ticket" and "get ticket by id" tested.

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,13 +2,15 @@
 # uvicorn app.main:app --reload برای اینمکه فست ای پی ای رو اجرا کنه 
 
 from fastapi import FastAPI
-
+from app.api.routes.tickets import router as ticket_router
 app = FastAPI(
     title="ServiceDesk Radar API",
     version="1.0.0",
     description="Backend API for ServiceDesk Radar"
 )
 
+app.include_router(ticket_router, prefix="/api")
+
 @app.get("/health")
 def health_check():
-    return {"status": "ok"}
+    return {"status": "I'm alive!"}
